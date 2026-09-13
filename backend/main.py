0045c8733fa11d907c88d7f7f8ba5b002bee637a
Eesha Upasya Mishra <154024644+eesha95@users.noreply.github.com>
2026-09-13 21:24:22 +0530

    Fix frontend API connection

--- backend/main.py
+++ backend/main.py
@@ -21,9 +21,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    "http://localhost:5173",
+    "https://jobtracker-4k47.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
